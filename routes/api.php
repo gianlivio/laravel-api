69d1c13c531fc3abb0f707e097cee5d3c41c380e
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-06-29 13:48:27 +0200

    scaffolding + projectcontroller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,25 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+// */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// elimino o commento questa route di default
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// passo l'array associativo al json con una di queste due sintassi
+
+// Route::get('/test', function() {
+//     return response()->json([
+//         'name' => 'Pippo',
+//         'role' => 'student'
+//     ]);
+// });
+
+// oppure
+
+Route::get('/projects', [ProjectController::class, 'index']);
